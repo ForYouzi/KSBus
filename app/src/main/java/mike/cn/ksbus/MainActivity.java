0e3da9bd6770a22d381dc34234c05098cb28c08e
MikeCoder <mike@mikecoder.cn>
2017-09-06 19:48:23 +0800

    init project

---
+++ app/src/main/java/mike/cn/ksbus/MainActivity.java
@@ -0,0 +1,78 @@
+package mike.cn.ksbus;
+
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.KeyEvent;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.RelativeLayout;
+
+import com.just.library.AgentWeb;
+
+import mike.cn.ksbus.config.Config;
+
+public class MainActivity extends AppCompatActivity {
+
+    private AgentWeb agentWeb;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        RelativeLayout layout = (RelativeLayout) findViewById(R.id.layout);
+
+        agentWeb = AgentWeb.with(this)
+                           .setAgentWebParent(layout, new RelativeLayout.LayoutParams(-1, -1))
+                           .useDefaultIndicator()
+                           .defaultProgressBarColor()
+                           .setWebViewClient(mWebViewClient)
+                           .addJavascriptInterface("local_obj", new InJavaScriptLocalObj())
+                           .createAgentWeb()
+                           .go("http://wap.ksbus.com.cn/mapHome");
+    }
+
+    //WebViewClient
+    private WebViewClient mWebViewClient = new WebViewClient() {
+        @Override
+        public void onPageStarted(WebView view, String url, Bitmap favicon) {
+            super.onPageStarted(view, url, favicon);
+            agentWeb.getJsEntraceAccess().callJs(getOutCss(Config.CSS_URL));
+        }
+
+
+        @Override
+        public void onPageFinished(WebView view, final String url) {
+            super.onPageFinished(view, url);
+            agentWeb.getJsEntraceAccess().callJs(getOutCss(Config.CSS_URL));
+        }
+    };
+
+    final class InJavaScriptLocalObj {
+        public void showSource(String html) {
+            System.out.println("====>html=" + html);
+        }
+    }
+
+    public static String getOutCss(String url) {
+        String js = ""; js = "javascript:var d=document;" +
+                             "var s=d.createElement('link');" +
+                             "s.setAttribute('rel', 'stylesheet');" +
+                             "s.setAttribute('href', '" + url + "');" +
+                             "d.head.appendChild(s);"; return js;
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if ((keyCode == KeyEvent.KEYCODE_BACK)) {
+            if (agentWeb.getWebCreator().get().canGoBack()) {
+                agentWeb.getWebCreator().get().goBack();
+                return true;
+            } else {
+                return false;
+            }
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+};
---
+++ app/src/main/java/mike/cn/ksbus/config/Config.java
@@ -0,0 +1,19 @@
+package mike.cn.ksbus.config;
+
+/**
+ * @author Mike
+ * @project KSBus
+ * @date 06/09/2017, 6:28 PM
+ * @e-mail mike@mikecoder.cn
+ */
+
+public class Config {
+    public static final String CSS = "#header_in{display:none}"
+                                     + "#footer{display:none} "
+                                     + " #gotoTop{display:none!important} "
+                                     + "#body_in{min-width:320px}"
+                                     + "marquee{border-radius:4px;background:0 0;color:#000;font-size:10px}"
+                                     + "#weather_index{display:none!important}";
+
+    public static final String CSS_URL = "http://47.92.122.133/KSBus/clean.css";
+}
